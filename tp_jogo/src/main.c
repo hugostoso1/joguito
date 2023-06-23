6093813ae51bd18d87d61fe44276e3975cf4ca9c
Rafael Tavares <rafatavares03@hotmail.com>
2023-06-23 14:34:41 -0300

    lendo nickname

--- tp_jogo/src/main.c
+++ tp_jogo/src/main.c
@@ -11,8 +11,35 @@ int main(void)
 
     InitWindow(game.screenWidth, game.screenHeight, "Aedsinho's quest");
     SetTargetFPS(60);
-
     InitGame(&game);
+
+    char nickname[50];
+    int countNick = 0;
+    while (!IsKeyDown(KEY_ENTER) && !WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLUE);
+        DrawText("ESCREVA UM NICKNAME", GetScreenWidth() / 2 - MeasureText("ESCREVA UM NICKNAME", 20) / 2, GetScreenHeight() / 2 - 50, 20, WHITE);
+        if(countNick) {
+            DrawText(nickname, GetScreenWidth() / 2 - MeasureText(nickname, 20) / 2, GetScreenHeight()- 150, 20, WHITE);
+        }
+        EndDrawing();
+        int key = GetCharPressed();
+        while (key > 0){
+            if ((key >= 32) && (key <= 125) && (countNick < 50)){
+                    nickname[countNick] = (char)key;
+                    nickname[countNick+1] = '\0';
+                    countNick++;
+                }
+
+                key = GetCharPressed();
+            }
+
+        if (IsKeyPressed(KEY_BACKSPACE)){
+            countNick--;
+            if (countNick < 0) countNick = 0;
+            nickname[countNick] = '\0';
+        }
+    }
 
     while (!WindowShouldClose()) // Detect window close button or ESC key
     {
