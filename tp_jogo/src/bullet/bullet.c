410979fae8a977e159e1cddee48b2c5ea73c73e2
hugostoso1 <136270146+hugostoso1@users.noreply.github.com>
2023-06-24 17:32:16 -0300

    bullet functionaç

--- tp_jogo/src/bullet/bullet.c
+++ tp_jogo/src/bullet/bullet.c
@@ -1,5 +1,6 @@
-#include "raylib.h"
+#include "../raylib.h"
 #include "../game/game.h"
+#include <stdio.h>
 
 void shoot(Bullet *b, Rectangle *position, Game *g) {
     if(IsKeyPressed(KEY_SPACE) &&  b->active == 0){
@@ -63,57 +64,8 @@ void update_bullet_pos(Bullet *b, Game *g){
 }
 
 void shootEnemy(Bullet *b, Rectangle *position, Game *g) {
-    if(b->active == 0){
-        b->active = 1;
-    }else {
-        update_enemyBullet_pos(b,g);
+    if(b->active){
+        update_bullet_pos(b,g);
     }
 }
 
-void update_enemyBullet_pos(Bullet *b, Game *g){
-    if(b->direction == KEY_LEFT) {
-            b->pos.x -= b->speed;
-        if (!(b->pos.x > SCREEN_BORDER)) {
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-    }
-    if(b->direction == KEY_RIGHT) {
-        b->pos.x += b->speed;
-        if (!(b->pos.x + b->pos.width < g->screenWidth - SCREEN_BORDER)){
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-    }
-    if(b->direction == KEY_UP) {
-        b->pos.y -= b->speed;
-        if (!(b->pos.y > SCREEN_BORDER)) {
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-    }
-    if(b->direction == KEY_DOWN) {
-        b->pos.y += b->speed;
-        if (!(b->pos.y +b->pos.height < g->screenHeight - SCREEN_BORDER)){
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
-            b->active = 0;
-            b->pos = b->default_pos;
-        }
-    }
-
-}
