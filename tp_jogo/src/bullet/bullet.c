b542fc82a6f517d2a98eef503a16b2801fea69fd
hugostoso1 <136270146+hugostoso1@users.noreply.github.com>
2023-06-24 16:32:36 -0300

    inimgo bala

--- tp_jogo/src/bullet/bullet.c
+++ tp_jogo/src/bullet/bullet.c
@@ -14,6 +14,7 @@ void shoot(Bullet *b, Rectangle *position, Game *g) {
         update_bullet_pos(b,g);
     }
 }
+
 void update_bullet_pos(Bullet *b, Game *g){
     if(b->direction == KEY_LEFT) {
             b->pos.x -= b->speed;
@@ -59,4 +60,60 @@ void update_bullet_pos(Bullet *b, Game *g){
             b->pos = b->default_pos;
         }
     }
+}
+
+void shootEnemy(Bullet *b, Rectangle *position, Game *g) {
+    if(b->active == 0){
+        b->active = 1;
+    }else {
+        update_enemyBullet_pos(b,g);
+    }
+}
+
+void update_enemyBullet_pos(Bullet *b, Game *g){
+    if(b->direction == KEY_LEFT) {
+            b->pos.x -= b->speed;
+        if (!(b->pos.x > SCREEN_BORDER)) {
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+    }
+    if(b->direction == KEY_RIGHT) {
+        b->pos.x += b->speed;
+        if (!(b->pos.x + b->pos.width < g->screenWidth - SCREEN_BORDER)){
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+    }
+    if(b->direction == KEY_UP) {
+        b->pos.y -= b->speed;
+        if (!(b->pos.y > SCREEN_BORDER)) {
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+    }
+    if(b->direction == KEY_DOWN) {
+        b->pos.y += b->speed;
+        if (!(b->pos.y +b->pos.height < g->screenHeight - SCREEN_BORDER)){
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+        if(barrier_collision(&g->maps[g->curr_map],&b->pos)){
+            b->active = 0;
+            b->pos = b->default_pos;
+        }
+    }
+
 }
