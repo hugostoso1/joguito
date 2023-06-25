0196983b5b88732ed838e203c7d8fed23d75a27a
hugostoso1 <136270146+hugostoso1@users.noreply.github.com>
2023-06-25 15:48:29 -0300

    Add files via upload

---
+++ joguito/joguito-main/tp_jogo/src/enemy/enemy.c
@@ -0,0 +1,91 @@
+#include "../raylib.h"
+#include <stdlib.h>
+#include "../game/game.h"
+
+void update_enemy_pos(Game *g, Enemy *e)
+{
+    Map *m = &g->maps[g->curr_map];
+    if(!e->enemyBullet.active){
+        int x = g->hero.pos.x - e->pos.x;
+        int y = g->hero.pos.y - e->pos.y;
+    
+        if(x>=-10 && x<=10){
+            e->enemyBullet.direction = (y>0) ? KEY_DOWN : KEY_UP;   
+            e->enemyBullet.pos = (Rectangle){e->pos.x, e->pos.y, 15, 45};
+            e->enemyBullet.active = 1;
+        }
+        if(y>=-10 && y<=10){
+            e->enemyBullet.direction = (x>0) ? KEY_RIGHT : KEY_LEFT;  
+            e->enemyBullet.pos = (Rectangle){e->pos.x, e->pos.y, 45, 15};
+            e->enemyBullet.active = 1;
+        }
+    }
+    
+
+    else if(e->enemyBullet.active){
+        update_enemyBullet_pos(&e->enemyBullet, g);
+    }
+
+    if(e->draw_enemy == 0){
+        e->enemyBullet.pos = e->enemyBullet.default_pos;
+        e->enemyBullet.active = 0;
+    }
+
+    
+
+    if (e->direction == KEY_LEFT)
+    {
+        if (e->pos.x > SCREEN_BORDER)
+            e->pos.x -= e->speed;
+        else
+        {
+            e->direction = KEY_RIGHT + (rand() % 4);
+        }
+        if (barrier_collision(m, &e->pos))
+        {
+            e->pos.x += e->speed;
+            e->direction = KEY_RIGHT + (rand() % 4);
+        }
+    }
+    else if (e->direction == KEY_RIGHT)
+    {
+        if (e->pos.x + e->pos.width < g->screenWidth - SCREEN_BORDER)
+            e->pos.x += e->speed;
+        else
+            e->direction = KEY_RIGHT + (rand() % 4);
+
+        if (barrier_collision(m, &e->pos))
+        {
+            e->pos.x -= e->speed;
+            e->direction = KEY_RIGHT + (rand() % 4);
+        }
+    }
+    else if (e->direction == KEY_UP)
+    {
+        if (e->pos.y > SCREEN_BORDER)
+            e->pos.y -= e->speed;
+        else
+            e->direction = KEY_RIGHT + (rand() % 4);
+
+        if (barrier_collision(m, &e->pos))
+        {
+            e->pos.y += e->speed;
+            e->direction = KEY_RIGHT + (rand() % 4);
+        }
+    }
+    else if (e->direction == KEY_DOWN)
+    {
+        if (e->pos.y + e->pos.height < g->screenHeight - SCREEN_BORDER)
+            e->pos.y += e->speed;
+        else
+            e->direction = KEY_RIGHT + (rand() % 4);
+
+        if (barrier_collision(m, &e->pos))
+        {
+            e->pos.y -= e->speed;
+            e->direction = KEY_RIGHT + (rand() % 4);
+        }
+    }
+    if (rand() % 100 == 1) // 1% de chance do inimigo mudar de direcao
+        e->direction = KEY_RIGHT + (rand() % 4);
+}
---
+++ joguito/joguito-main/tp_jogo/src/main.c
@@ -0,0 +1,80 @@
+#include "raylib.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "./game/game.h"
+
+int main(void)
+{
+    Game game;
+    game.screenWidth = 800;
+    game.screenHeight = 480;
+
+    InitWindow(game.screenWidth, game.screenHeight, "Aedsinho's quest");
+    SetTargetFPS(60);
+    InitGame(&game);
+
+    char nickname[50];
+    int countNick = 0;
+    int select = 1; // 1 normal, 0 hard
+    while (!IsKeyPressed(KEY_ENTER) && !WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLUE);
+        DrawText("ESCREVA UM NICKNAME", GetScreenWidth() / 2 - MeasureText("ESCREVA UM NICKNAME", 20) / 2, GetScreenHeight() / 2 - 50, 20, BLACK);
+        if(countNick) {
+            DrawText(nickname, GetScreenWidth() / 2 - MeasureText(nickname, 20) / 2, GetScreenHeight()- 150, 20, BLACK);
+        }
+        EndDrawing();
+        int key = GetKeyPressed();
+        while (key > 0){
+            if ((key >= 32) && (key <= 125) && (countNick < 50)){
+                    nickname[countNick] = (char)key;
+                    nickname[countNick+1] = '\0';
+                    countNick++;
+                }
+            key = GetKeyPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE)){
+            countNick--;
+            if (countNick < 0) countNick = 0;
+            nickname[countNick] = '\0';
+        }
+    }
+    
+    
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLUE);
+        DrawText("ESCOLHA UM MODO DE JOGO:", GetScreenWidth() / 2 - MeasureText("ESCOLHA UM MODO DE JOGO:", 20) / 2, GetScreenHeight() / 2 - 75, 20, BLACK);
+        if(IsKeyDown(KEY_DOWN)) {
+            select = 0;
+        } else if (IsKeyDown(KEY_UP)) {
+            select = 1;
+        }
+
+        if(select) {
+            DrawText("NORMAL GAME", GetScreenWidth() / 2 - MeasureText("NORMAL GAME", 25) / 2, GetScreenHeight() - 200, 25, WHITE);
+            DrawText("HARD GAME", GetScreenWidth() / 2 - MeasureText("HARD GAME", 20) / 2, GetScreenHeight() - 150, 20, BLACK);
+        } else {
+            DrawText("NORMAL GAME", GetScreenWidth() / 2 - MeasureText("NORMAL GAME", 20) / 2, GetScreenHeight() - 200, 20, BLACK);
+            DrawText("HARD GAME", GetScreenWidth() / 2 - MeasureText("HARD GAME", 25) / 2, GetScreenHeight() - 150, 25, WHITE);
+        }
+        EndDrawing();
+        if(IsKeyPressed(KEY_ENTER)) break;
+    }
+
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        UpdateDrawFrame(&game);
+        if (game.gameover)
+            break;
+    }
+    while (!IsKeyDown(KEY_ENTER) && !WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("GAME OVER", GetScreenWidth() / 2 - MeasureText("GAME OVER", 20) / 2, GetScreenHeight() / 2 - 50, 20, BLACK);
+        EndDrawing();
+    }
+    return 0;
+}
